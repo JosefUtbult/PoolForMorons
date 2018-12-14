7c377424313a98968dcb79b0542984944a615269
josef <josutb-7@student.ltu.se>
2018-12-14 14:59:50 +0100

    Added a Pool_ball class

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 # Welcome to the begining of the file
 
+import pygame
+
+class Pool_ball:
+    def __init__(self, position: tuple, color: pygame.Color):
+        self.position = position
+        self.color = color
+
 def main():
-    pass
+    
+    baluru = Pool_ball((0, 0), pygame.Color(255, 0, 0))
 
 if __name__ == "__main__":
     main()
