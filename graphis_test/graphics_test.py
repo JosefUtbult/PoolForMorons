7bf9ba7180f0157b373f42193c1f70b7bba2a89e
josef <josutb-7@student.ltu.se>
2018-12-23 20:40:51 +0100

    Got the rotation of the sphere working in 3 dimentions

--- graphis_test/graphics_test.py
+++ graphis_test/graphics_test.py
@@ -3,10 +3,10 @@ from math import pi, sin, cos, tan, radians, degrees, sqrt, floor, atan
 from time import sleep
 screen = None
 overall_radius = 200
-nr_of_layers = 20
+nr_of_layers = 10
 
 origin = [400, 400]
-
+solid = False
 
 def main():
     global screen
@@ -27,11 +27,11 @@ def main():
         pygame.draw.circle(screen, pygame.Color(255, 0, 0), spheres[0].position[:2], 3)
         pygame.display.flip()
     
-        spheres[0].move([delta, delta])
+        spheres[0].move([delta * 2, delta])
 
-        if spheres[0].position[0] == 1000:
+        if spheres[0].position[0] >= 1000:
             delta = -1
-        elif spheres[0].position[0] == 300:
+        elif spheres[0].position[0] <= 400:
             delta = 1
         
 
@@ -47,9 +47,14 @@ class Sphere:
         
         for polygon in self.polygons:
 
+            if not solid:
+                pygame.draw.polygon(screen, polygon.color, [(int(polygon.nodes[i].position[0] + self.position[0]), int(polygon.nodes[i].position[1] + self.position[1])) for i in range(len(polygon.nodes))], 1)
+            else:
+                pygame.draw.polygon(screen, polygon.color, [(polygon.nodes[i].position[0] + self.position[0], polygon.nodes[i].position[1] + self.position[1]) for i in range(len(polygon.nodes))])
+            
+            if polygon is self.polygons[0]:
+                print(polygon.nodes[0].position[2])
 
-            pygame.draw.polygon(screen, polygon.color, [(polygon.nodes[i].position[0] + self.position[0], polygon.nodes[i].position[1] + self.position[1]) for i in range(len(polygon.nodes))], 1)
-    
     def move(self, distance):
         global overall_radius
 
@@ -60,23 +65,24 @@ class Sphere:
             for node in self.nodes[y]:
                 node.position[0] = cos(angle[0]) * node.position[0] - sin(angle[0]) * node.position[1]
                 
-                # node.position[1] = sin(angle[0]) * node.position[0] + cos(angle[0]) * node.position[1] + sin(angle[1]) * node.position[2] + cos(angle[1]) * node.position[1]
- 
                 node.position[1] = sin(angle[0]) * node.position[0] + cos(angle[0]) * node.position[1]
                 node.position[1] = sin(angle[1]) * node.position[2] + cos(angle[1]) * node.position[1]
                 
                 node.position[2] = cos(angle[1]) * node.position[2] - sin(angle[1]) * node.position[1]
                
                 #if sqrt(pow(node.position[0], 2) + pow(node.position[1], 2) + pow(node.position[2], 2)) < overall_radius:
+                #
+                #    temp = abs(node.position[0]) + abs(node.position[1]) + abs(node.position[2])
+                #    
                 #    for i in range(3):
-                #        if node.position[i] < 0:
-                #            node.position[i] -= 1
-                #        elif node.position[i] > 0:
-                #            node.position[i] += 1
+                #        
+                #        node.position[i] += node.position[i] / temp
+                #
+                #            self.position[0] += distance[0]
+        
 
         self.position[0] += distance[0]
         self.position[1] += distance[1]
-
        
 
 
